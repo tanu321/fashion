3d4010933d00561679a90af8414c74578e72bdf6
tanu321 <tammana1078@gmail.com>
2016-09-23 11:05:59 +0530

    update

--- secret/client/main.js
+++ secret/client/main.js
@@ -1,96 +1,73 @@
-Camera = function (videoTagSelector, options) {
+Template.camera.events({
+    'click #jadoo':function(){
+     console.log("abc");
+    Webcam.on( 'error', function(err) {
+        console.log(err); // outputs error to console instead of window.alert
+    });
 
-	this._iniShims = function() {
-
-		if (!navigator.getUserMedia) {
-			this.fail();
-			return false;
-		}
-		return true;
-	}
-
-	this.setOnSuccess = function(callback) {
-		if(!this.cameraRunning) {
-			this.success = callback;
-			return;
-		}
-		callback(this);
-	}
-
-	this.init = function(){
-
-		this.fail = options.fail || function() { console.log('user has not accepted camera input'); };
-		this.success = options.success || function() { console.log('camera input has started'); };
-
-		if (!this._iniShims()) return false;
-
-
-		this.videoObject = typeof videoTagSelector == 'string' ? $(videoTagSelector): videoTagSelector;
-		this.video = this.videoObject[0];
-
-		this.video.height = options.height || this.videoObject.height();
-		this.video.width = options.width || this.videoObject.width();
-
-		// set up stream
-		navigator.getUserMedia(
-			window.userMediaVideoSelector,
-			_.bind(
-				function (stream) {
-					if (this.video.mozCaptureStream) {
-						this.video.mozSrcObject = stream;
-					} else {
-						this.video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-					}
-					this.video.play();
-					this.stream = stream;
-					this.success(this);
-					this.cameraRunning = true;
-				},
-				this
-			),
-			this.fail
-		);
-	}
-
-	this.attachCanvas = function(options){
-
-		var cameraCanvas = options.canvas || document.createElement('canvas');
-	    cameraCanvas.width = options.width || this.video.width;
-	    cameraCanvas.height = options.height || this.video.height;
-	    var cameraCanvasContext = cameraCanvas.getContext('2d');
-	    var onUpdate =  options.onUpdate || function(){};
-
-	    // do this every x seconds
-	    setInterval(
-	    	_.bind(
-	    	function(){
-	    		//draw image in temp canvas
-	    		cameraCanvasContext.drawImage(this.video, 0, 0, cameraCanvas.width, cameraCanvas.height);
-	    		onUpdate(cameraCanvas, cameraCanvasContext);
-	    		return;
-	    	},
-	    	this),
-	    	options.period || 1000
-	    );
-
-	    return cameraCanvas;
-	}
-
-	this.init();
-
-	return this;
+    Webcam.set({
+        width: 320,
+        height: 240,
+        dest_width: 640,
+        dest_height: 480,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+    Webcam.attach( '#webcam' );
 }
+});
 
-Template.camera.rendered = function(){
-	var cameraNode = $(this.firstNode);
-	cameraNode[0].camera = new Camera(cameraNode, {});
-};
-
-Template.camera.helpers(
-    {
-        cameraId: function() {
-            return _.uniqueId('camera');
-        }
+Template.camera.events({
+    'click .snap': function () {
+        Webcam.snap( function(image) {
+            Session.set('webcamSnap', image);	
+        })
     }
-);
+});
+
+Template.camera.helpers({
+    image: function () {
+        return Session.get('webcamSnap');
+    }
+});
+
+if (Meteor.isClient) {
+
+  Meteor.startup(function() {
+
+    // This assigns a file upload drop zone to some DOM node
+    myFiles.resumable.assignDrop($(".fileDrop"));
+
+    // This assigns a browse action to a DOM node
+    myFiles.resumable.assignBrowse($(".fileBrowse"));
+
+    // When a file is added via drag and drop
+    myFiles.resumable.on('fileAdded', function (file) {
+
+      // Create a new file in the file collection to upload
+      myFiles.insert({
+        _id: file.uniqueIdentifier,  // This is the ID resumable will use
+        filename: file.fileName,
+        contentType: file.file.type
+        },
+        function (err, _id) {  // Callback to .insert
+          if (err) { return console.error("File creation failed!", err); }
+          // Once the file exists on the server, start uploading
+          myFiles.resumable.upload();
+        }
+      );
+    });
+
+    // This autorun keeps a cookie up-to-date with the Meteor Auth token
+    // of the logged-in user. This is needed so that the read/write allow
+    // rules on the server can verify the userId of each HTTP request.
+    Deps.autorun(function () {
+      // Sending userId prevents a race condition
+      Meteor.subscribe('myData', Meteor.userId());
+      // $.cookie() assumes use of "jquery-cookie" Atmosphere package.
+      // You can use any other cookie package you may prefer...
+      $.cookie('X-Auth-Token', Accounts._storedLoginToken(), { path: '/' });
+    });
+  });
+}
 
